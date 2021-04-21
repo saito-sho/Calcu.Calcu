fb17255b778152fcdc7bad262e706eac643e077a
saito-sho <hyakuhati17@gmail.com>
2021-04-21 21:17:20 +0900

    変更

--- calcu.php
+++ calcu.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
 <html lang=“ja”>
 <meta http-equiv="content-type" charset="UTF-8">
+<h1>結果！！！！</h1>
 <h1>
     <?php echo $_POST['number1']  ?>
+    <?php echo $_POST['number2']  ?>
+
 </h1>
 
 </html>

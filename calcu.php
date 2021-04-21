2c12fda19c9527e6e05989286c3fca0a43c2f33f
saito-sho <hyakuhati17@gmail.com>
2021-04-21 21:08:45 +0900

    変更

--- calcu.php
+++ calcu.php
@@ -2,18 +2,6 @@
 <html lang=“ja”>
 <meta http-equiv="content-type" charset="UTF-8">
 
-<form action="calcu.php" method="POST">
-    <p>
-        計算:
-        数字1<input type="text" name="number1" size="40">
-        数字2<input type="text" name="number2" size="40">
-    </p>
-</form>
-<form action="calcu.php" method="post">
-    <p>
-        <input type="submit" value="計算！！！！">
-    </p>
-    <p>加算：</p>
-</form>
+<?php echo $_POST['number1'] + $_POST['number2']?>
 
 </html>
